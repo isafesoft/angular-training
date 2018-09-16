5617cc08a32225c6e13e63c84613f9b49731642b
Mark Xu <isafesoft@hotmail.com>
2018-09-16 14:01:29 -0400

    ngclass and className binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ interface Passenger {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   passengers: Passenger[] = [{
