aea07bfec598f9619fb61efd9125fc637fb89c51
Mark Xu <mark.xu@itsp-inc.com>
2018-09-18 14:45:13 -0400

    Presentational components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     // angular modules
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './containers/dashboard/dashboard.component';
+import {CountComponent} from './component/count/count.component';
+import {DetailComponent} from './component/detail/detail.component';
 
 // module configuration
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [DashboardComponent],
+  declarations: [
+    DashboardComponent,
+    CountComponent,
+    DetailComponent
+  ],
   exports: [
-    DashboardComponent
+    DashboardComponent,
   ]
 })
 export class DashboardModule { 
