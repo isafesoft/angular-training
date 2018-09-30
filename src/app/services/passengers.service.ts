39ff8119352af830c0914d826b975f5b8984adf9
Mark Xu <isafesoft@hotmail.com>
2018-09-29 23:24:41 -0400

    custom http headers

--- src/app/services/passengers.service.ts
+++ src/app/services/passengers.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import {Passenger} from '../models/passenger';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {RequestOptions} from '@angular/http';
 
 const HOST_URL = 'http://localhost:3000';
 const PASSENGER_API = '/passengers';
@@ -19,7 +20,14 @@ export class PassengersService {
   }
 
   updatePassenger(passenger: Passenger): Observable<Object> {
-    return this.http.put(HOST_URL + PASSENGER_API + '/' + passenger.id, passenger);
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'my-auth-token'
+      })
+    };
+    return this.http.put(HOST_URL + PASSENGER_API + '/' + passenger.id, passenger, httpOptions);
   }
 
   removePassenger(passenger: Passenger): Observable<Object> {
