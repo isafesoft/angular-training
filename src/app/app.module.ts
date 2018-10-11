59a7fc3487f7b4881908f2856799a0b8b05126c0
Mark Xu <isafesoft@hotmail.com>
2018-10-11 17:10:59 -0400

    Root module routes and router-outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {PassengersService} from './services/passengers.service';
+import {RouterModule, Routes} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent, pathMatch: 'full'}
+]
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
   ],
   imports: [
     // angular modules
     BrowserModule,
+    RouterModule.forRoot(routes),
     // custom modules
     DashboardModule
   ],
