aaad52125468000019fe1549cdb1f6264c236aae
Mark Xu <isafesoft@hotmail.com>
2018-10-11 17:33:29 -0400

    Wildcard 404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,17 @@ import { AppComponent } from './app.component';
 import {PassengersService} from './services/passengers.service';
 import {RouterModule, Routes} from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent, pathMatch: 'full'}
+  {path: '**', component: NotFoundComponent}
 ]
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    NotFoundComponent,
   ],
   imports: [
     // angular modules
