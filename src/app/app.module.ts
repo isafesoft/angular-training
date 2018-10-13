554c04f5a00001f65670b6fee66da2d50bece654
Mark Xu <isafesoft@hotmail.com>
2018-10-12 22:57:33 -0400

    routerLink

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent, pathMatch: 'full'}
+  {path: '', component: HomeComponent, pathMatch: 'full'},
   {path: '**', component: NotFoundComponent}
 ]
 @NgModule({
