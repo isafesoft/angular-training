e280f1683a1626da7fa438dc3a9c27cc80ab6efb
Mark Xu <isafesoft@hotmail.com>
2018-10-14 00:50:23 -0400

    Route params, switchMap and route params

--- src/app/dashboard/containers/dashboard-viewer/dashboard-viewer.component.ts
+++ src/app/dashboard/containers/dashboard-viewer/dashboard-viewer.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Passenger } from '../../../models/passenger';
 import {PassengersService} from '../../../services/passengers.service';
-
+import {ActivatedRoute, ParamMap, Params, Router} from '@angular/router';
+import {switchMap} from 'rxjs/operators';
 @Component({
   selector: 'app-dashboard-viewer',
   templateUrl: './dashboard-viewer.component.html',
@@ -10,10 +11,15 @@ import {PassengersService} from '../../../services/passengers.service';
 export class DashboardViewerComponent implements OnInit {
 
   passenger: Passenger = null;
-  constructor(private passengerService: PassengersService) { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private passengerService: PassengersService) { }
 
   ngOnInit() {
-    this.passengerService.getPassenger(1).subscribe(obj => this.passenger = <Passenger>obj)
+    this.route.paramMap.pipe(switchMap((params: ParamMap) =>
+        this.passengerService.getPassenger(parseInt(params.get('id'), 10))
+      )).subscribe(obj => this.passenger = <Passenger>obj)
   }
 
   onUpdatePassenger(p: Passenger) {
